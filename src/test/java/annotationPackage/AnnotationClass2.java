87b667facafbb8b8a3696adc494e1210b533eba6
nandeeshcl <nandeeshclakhi@gmail.com>
2020-03-28 20:40:01 +0530

    annotations

---
+++ src/test/java/annotationPackage/AnnotationClass2.java
@@ -0,0 +1,43 @@
+package annotationPackage;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class AnnotationClass2 {
+	
+	@BeforeTest
+	public void openChrome()
+	{
+		System.out.println("OPen CHrome driver");
+	}
+	
+	
+	@AfterTest
+	public void closeChrome()
+	{
+		System.out.println("CLose CHrome driver");
+	}
+	
+	@Test
+	public void userProfile()
+	{
+		System.out.println("User Profile");
+	}
+	
+	@BeforeSuite
+	public void setup()
+	{
+		System.out.println("Setup method");
+	}
+	
+	
+	@AfterSuite
+	public void tearDown()
+	{
+		System.out.println("TearDown method");
+	}
+
+}
---
+++ src/test/java/annotationPackage/AnnotationsClass.java
@@ -0,0 +1,44 @@
+package annotationPackage;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AnnotationsClass {
+
+	@BeforeClass
+	public void openURL()
+	{
+		System.out.println("Open google url");
+	}
+	
+	@AfterClass
+	public void closeURL()
+	{
+		System.out.println("Close google URL");
+	}
+	
+	@BeforeMethod
+	public void login() {
+		System.out.println("Login to the application");
+	}
+
+	@AfterMethod
+	public void logout() {
+		System.out.println("Logout from the application");
+	}
+
+
+	@Test(priority=100)
+	public void deleteUser() {
+		System.out.println("Delete user...");
+	}
+	
+	@Test(priority=99)
+	public void createUser() {
+		System.out.println("Create new user...");
+	}
+
+}
